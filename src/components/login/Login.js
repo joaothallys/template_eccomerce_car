381aee04768416cf068e81594b5399211fb915c7
joaothallys <jlemes796@gmail.com>
2024-12-10 19:17:23 -0300

    clore: manutencao tela de login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,28 +1,37 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import CircularProgress from '@mui/material/CircularProgress';
 import './Login.style.css'; // Estilo da tela de login
 import { loginUser } from '../Services/apiServices'; // Importe o serviço de autenticação
 
 export const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
     const handleLogin = async (e) => {
         e.preventDefault();
+        setLoading(true);
         try {
             const response = await loginUser(email, password);
             if (response.status === 200) {
                 const data = await response.json();
                 localStorage.setItem('authToken', data.AccessToken); // Armazena o token no localStorage
                 localStorage.setItem('password', password);
-                localStorage.setItem('nome', data.nome); // Armazena a senha no localStorage
+                localStorage.setItem('nome', data.nome);
+                localStorage.setItem('id', data.id);
                 navigate('/manage');
+            } else if (response.status === 401) {
+                setErrorMessage('Senha inválida. Tente novamente.');
             } else {
                 console.error('Login failed');
             }
         } catch (error) {
             console.error('Error during login:', error);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -51,7 +60,10 @@ export const Login = () => {
                         required
                     />
                 </div>
-                <button type="submit">Login</button>
+                {errorMessage && <p className="error-message">{errorMessage}</p>}
+                <button type="submit" disabled={loading}>
+                    {loading ? <CircularProgress size={24} /> : 'Login'}
+                </button>
             </form>
         </div>
     );
